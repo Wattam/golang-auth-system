efeb48f24db371b206eacda776cb3f45d0699a13
Nalbert Wattam <nalbertwattam@gmail.com>
2022-02-10 21:34:57 -0300

    chore: update shoe model

--- model/shoe.go
+++ model/shoe.go
@@ -9,9 +9,7 @@ import (
 type Shoe struct {
 	ID        uint           `json:"id,string"`
 	Name      string         `json:"name" gorm:"not null; default:null; check:Name != '' "`
-	Style     string         `json:"style" gorm:"not null; default:null; check:Style != '' "`
-	Colour    string         `json:"colour" gorm:"not null; default:null; check:Colour != '' "`
-	Material  string         `json:"material" gorm:"not null; default:null; check:Material != '' "`
+	Color     string         `json:"color" gorm:"not null; default:null; check:Color != '' "`
 	Price     float64        `json:"price,string" gorm:"not null; default:null; check:Price > 0 "`
 	CreatedAt time.Time      `json:"-" gorm:"<-:create"`
 	UpdatedAt time.Time      `json:"-"`
